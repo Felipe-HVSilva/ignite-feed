e70bf9a83a4095e61d8ef90b89a23dd73bd1f6be
Felipe-HVSilva <felype.hen@hotmail.com>
2023-05-07 18:12:54 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+<h1 align="center"> Ignite Feed </h1>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>&nbsp;&nbsp;&nbsp;
+ 
+</p>
+
+<p align="center">
+  <img alt="License" src="./ignite-feed.png">
+</p>
+<hr>
+
+<br>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- React
+- Typescript
+- Module CSS
+
+## Projeto
+
+Para acessar o projeto clique [aqui]()
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT.
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,29 @@
+import { PencilLine } from "phosphor-react"
+import styles from "./Sidebar.module.css"
+import { Avatar } from "./Avatar"
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1661956602944-249bcd04b63f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+        alt=""
+      />
+
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/felipe-hvsilva.png" />
+
+        <strong>Felipe Silva</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
